455f8e21a2cc46f85abf2bfaff6227c76e5b8471
anantlk <anant.lk.1997@gmail.com>
2018-04-16 23:24:58 +0530

    Add files via upload

---
+++ SendingEmailAfterMotionDetection.ino
@@ -0,0 +1,35 @@
+int pin1=7;
+int pin2=13;
+int TimeBetwEmail=15;
+long lastSend=-TimeBetwEmail*1000l;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin1,INPUT);
+  pinMode(pin2,OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  long now=millis();
+  int val=digitalRead(pin1);
+  if(val==HIGH)
+  {
+  if(now>(lastSend+TimeBetwEmail*1000l))
+  {
+    lastSend=now;
+    Serial.println("Movement");
+  }
+  else
+  {
+    Serial.println("Too soon");
+  }
+  digitalWrite(pin2,HIGH);
+  delay(1000);
+  }
+  else
+  {
+    Serial.println("No Movement");
+    digitalWrite(pin2,LOW);
+    delay(1000);
+  }
+}
